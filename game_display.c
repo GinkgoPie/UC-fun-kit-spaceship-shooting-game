cb6f475f8dd097495c461f3b6de5ed915c39a843
Jiahe Bao <jba214@cs19112bs.canterbury.ac.nz>
2022-10-17 23:33:37 +1300

    fixed display issue

--- game_display.c
+++ game_display.c
@@ -13,26 +13,56 @@
 
 
 
-void column_shift_right(uint8_t* led_map) {
-    uint8_t *copy = NULL;
-    *copy = led_map[LEDMAT_COLS];
-    led_map[4] = led_map[3] & 0xF0;
-    led_map[3] = led_map[2] & 0xF0 ;
-    led_map[2] = led_map[1] & 0xF0;
-    led_map[1] = led_map[0] & 0xF0;
-    led_map[0] = *copy & 0xF0;
+void column_shift_right(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row) {
+    // uint8_t *copy = NULL;
+    // *copy = led_map[LEDMAT_COLS];
+    // led_map[4] = led_map[3] & 0xF0;
+    // led_map[3] = led_map[2] & 0xF0 ;
+    // led_map[2] = led_map[1] & 0xF0;
+    // led_map[1] = led_map[0] & 0xF0;
+    // led_map[0] = *copy & 0xF0;
+
+    for (uint8_t i = ship_head_row; i<LEDMAT_ROWS+1;i++) {
+        single_pixel_set(led_map,ship_head_col,i,0);
+    }
+
+    if (ship_head_col == 4) {
+        ship_head_col = 0;
+    } else {
+        ship_head_col++;
+    }
+
+    for (uint8_t i = ship_head_row; i<LEDMAT_ROWS+1;i++) {
+        single_pixel_set(led_map,ship_head_col,i,1);
+    }
+
 
 }
 
 
-void column_shift_left(uint8_t* led_map) {
-    uint8_t *copy = NULL;
-    *copy = led_map[0];
-    led_map[0] = led_map[1]& 0xF0;
-    led_map[1] = led_map[2]& 0xF0;
-    led_map[2] = led_map[3]& 0xF0;
-    led_map[3] = led_map[4]& 0xF0;
-    led_map[4] = *copy& 0xF0;
+void column_shift_left(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row) {
+    // uint8_t *copy = NULL;
+    // *copy = led_map[0];
+    // led_map[0] = led_map[1]& 0xF0;
+    // led_map[1] = led_map[2]& 0xF0;
+    // led_map[2] = led_map[3]& 0xF0;
+    // led_map[3] = led_map[4]& 0xF0;
+    // led_map[4] = *copy& 0xF0;
+
+
+    for (uint8_t i = ship_head_row; i<LEDMAT_ROWS+1;i++) {
+        single_pixel_set(led_map,ship_head_col,i,0);
+    }
+
+    if (ship_head_col == 0) {
+        ship_head_col = LEDMAT_COLS;
+    } else {
+        ship_head_col--;
+    }
+
+    for (uint8_t i = ship_head_row; i<LEDMAT_ROWS+1;i++) {
+        single_pixel_set(led_map,ship_head_col,i,1);
+    }
 }
 
 void update_ledmat (uint8_t* led_map)
