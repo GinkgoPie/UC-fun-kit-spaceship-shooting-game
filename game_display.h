6d0d0385bfdc65758e882811c511dc1ab3347fd2
Callum Whitehead <cwh74@uclive.ac.nz>
2022-10-18 01:16:08 +0000

    Code commented

--- game_display.h
+++ game_display.h
@@ -1,7 +1,7 @@
-/** @file   ship.h
-    @author Jiahe Bao
-    @date  15 october 2022
-    @brief  Updating and displaying led matrix during the game
+/** @file   game_display.h
+    @author Jiahe Bao, Callum Whitehead
+    @date   01 October 2022
+    @brief  The header file for game_display.c
 
     @defgroup
 */
@@ -12,20 +12,13 @@
 #include "system.h"
 
 
-
-
 void column_shift_right(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row);
 
-
 void column_shift_left(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row);
-
 
 void update_ledmat(uint8_t* led_map);
 
 void single_pixel_set (uint8_t* display,uint8_t col, uint8_t row, bool val);
-
-
-
 
 
 #endif
