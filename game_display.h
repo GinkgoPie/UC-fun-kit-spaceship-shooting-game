ec1016bef3de216862dc550d11bc735e35912f18
Callum Whitehead <cwh74@uclive.ac.nz>
2022-10-18 02:56:49 +0000

    Update game_display.h

--- game_display.h
+++ game_display.h
@@ -11,13 +11,16 @@
 
 #include "system.h"
 
-
+//Function that shifts the 'ship' right on the LED Matrix.
 void column_shift_right(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row);
 
+//Works the same as the column_shift_right function but shifts left by decreasing the value of the ships column
 void column_shift_left(uint8_t* led_map, uint8_t ship_head_col,uint8_t ship_head_row);
 
+//Constantly updates the LED Matrix with the players' ships current position
 void update_ledmat(uint8_t* led_map);
 
+//Sets a singular pixel to be either on or off depending on the row and column of the pixel in the LED Matrix
 void single_pixel_set (uint8_t* display,uint8_t col, uint8_t row, bool val);
 
 
